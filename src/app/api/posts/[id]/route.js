395d1e35aca62d9c5849c52df396961350872c76
Temuulen Narmandakh <temkanibno@gmail.com>
2024-10-16 17:06:23 +0800

    replace fs by prisma

--- src/app/api/posts/[id]/route.js
+++ src/app/api/posts/[id]/route.js
@@ -1,22 +1,17 @@
-import { nanoid } from "nanoid";
+import { PrismaClient } from "@prisma/client";
 import { NextResponse } from "next/server";
-import fs from "fs/promises";
-
-const filePath = "./data/posts.json";
 
 export const GET = async (_, { params }) => {
   const { id } = params;
 
   if (!id) return NextResponse.json({ message: "id хоосон байж болохгүй" }, { status: 400 });
   try {
-    const fileData = await fs.readFile(filePath, "utf-8");
-    const posts = JSON.parse(fileData);
-
-    const post = posts.find((post) => post.id === id);
+    const prisma = new PrismaClient();
+    const post = await prisma.post.findUnique({ where: { uid: id } });
 
     if (!post) return NextResponse.json({ message: "Мэдээ олдсонгүй" }, { status: 404 });
 
-    return NextResponse.json(post);
+    return NextResponse.json({ ...post, id: post.uid });
   } catch (error) {
     return NextResponse.json({ message: "Алдаа гарлаа", error: error.message }, { status: 500 });
   }
