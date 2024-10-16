984b51573002d0829e13657930288230ea917740
Temuulen Narmandakh <temkanibno@gmail.com>
2024-10-16 17:45:24 +0800

    add cors

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,30 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/api/(.*)",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "localhost:3000 ",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "POST, PUT, DELETE, GET, OPTIONS",
+          },
+          {
+            key: "Access-Control-Request-Method",
+            value: "*",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Origin, X-Requested-With, Content-Type, Accept, Authorization",
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
